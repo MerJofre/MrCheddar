88520fead3e7f1be079652a7afd22f544191ef3b
Mercedes Jofre <mer.jofre@gmail.com>
2025-08-09 13:23:33 -0300

    agregado el menu

---
+++ script/main.js
@@ -0,0 +1,34 @@
+const opciones = [
+	{   nom: 'cheddar', 
+        ingredientes: 'Pan con queso, cheddarx3, medallon,salsa especial',
+        precio:'$7500',
+        id: 1,
+        imagen:'images\cheddar.png'
+    },
+	{ 
+        nom: 'bacon', 
+        ingredientes: 'Pan con queso, cheddar, medallon,bacon,cebolla caramelizada,salsa especial', 
+        precio:'$8500',
+        id: 2 },
+	{ 
+        nom: 'clasica', 
+        ingredientes: 'Pan con sesamo, muzarella, medallon,lechuga y tomate, mayo de ajo', 
+        precio:'$7500',
+        id: 3 },
+    { 
+        nom: 'napo', 
+        ingredientes: 'Pan con sesamo, muzarella, medallon,tomate,jamon cocido, mayo de ajo', 
+        precio:'$8000',
+        id: 4 },
+    { 
+        nom: 'big', 
+        ingredientes: 'Pan con sesamo, cheddar, medallon,pepinillos,lechuga, salsa especial', 
+        precio:'$9000',
+        id: 5 },
+    { 
+        nom: 'spicy', 
+        ingredientes: 'Pan con queso, cheddar, medallon, bacon,jalapeños,aji molido,huevo frito', 
+        precio:'$9000',
+        id: 6 },
+]  
+
