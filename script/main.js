f369750b6cae538d1135c28e247e5b8d11dac77f
Mercedes Jofre <mer.jofre@gmail.com>
2025-08-11 01:05:01 -0300

    agregada la funcion de carrito

--- script/main.js
+++ script/main.js
@@ -1,34 +1,112 @@
+const container = document.getElementById('container');
+const carritoFisico = document.getElementById("cart")
+const carritoBoton = document.getElementById("cart-btn")
+const cerrarCarrito = document.getElementById("close-cart")
+const carritoFisicoInterno = document.getElementById("carrito-fisico-interno")
+const total = document.getElementById('total')
+
+carritoBoton.addEventListener("click", () => {
+    carritoFisico.classList.toggle('show')
+})
+
+cerrarCarrito.addEventListener("click", () => {
+    carritoFisico.classList.remove('show')
+})
+
+let carrito = [];
+
 const opciones = [
-	{   nom: 'cheddar', 
+	{   nom: 'Cheddar', 
         ingredientes: 'Pan con queso, cheddarx3, medallon,salsa especial',
-        precio:'$7500',
+        precio:'7500',
         id: 1,
-        imagen:'images\cheddar.png'
+        imagen:'"https://iili.io/FQRwmGt.th.png"'
     },
 	{ 
-        nom: 'bacon', 
+        nom: 'Bacon', 
         ingredientes: 'Pan con queso, cheddar, medallon,bacon,cebolla caramelizada,salsa especial', 
-        precio:'$8500',
+        precio:'8500',
+        imagen:'"https://iili.io/FQRwmGt.th.png"',
         id: 2 },
 	{ 
-        nom: 'clasica', 
+        nom: 'Clasica', 
         ingredientes: 'Pan con sesamo, muzarella, medallon,lechuga y tomate, mayo de ajo', 
-        precio:'$7500',
+        precio:'7500',
+        imagen:'"https://iili.io/FQRwmGt.th.png"',
         id: 3 },
     { 
-        nom: 'napo', 
+        nom: 'Napo', 
         ingredientes: 'Pan con sesamo, muzarella, medallon,tomate,jamon cocido, mayo de ajo', 
-        precio:'$8000',
+        precio:'8000',
+        imagen:'"https://iili.io/FQRwmGt.th.png"',
         id: 4 },
     { 
-        nom: 'big', 
+        nom: 'Big', 
         ingredientes: 'Pan con sesamo, cheddar, medallon,pepinillos,lechuga, salsa especial', 
-        precio:'$9000',
+        precio:'9000',
+        imagen:'"https://iili.io/FQRwmGt.th.png"',
         id: 5 },
     { 
-        nom: 'spicy', 
+        nom: 'Spicy', 
         ingredientes: 'Pan con queso, cheddar, medallon, bacon,jalapeños,aji molido,huevo frito', 
-        precio:'$9000',
+        precio:'9000',
+        imagen:'"https://iili.io/FQRwmGt.th.png"',
         id: 6 },
 ]  
 
+const calculadoraTot = () => {
+    let total = carrito.reduce((acum,el) => {
+    return acum + Number(el.precio.replace('$', ''))
+
+    }, 0)
+
+    return total
+}
+
+const agregadoraCarrito = () => {
+    carritoFisicoInterno.innerHTML = ''
+    carrito.forEach((opcion) => {
+        carritoFisicoInterno.innerHTML += `<h3>${opcion.nom}</h3>
+        <p>$${opcion.precio}</p>`
+    })
+
+    let calculoTot = calculadoraTot()
+    
+    total.innerHTML = `<p> Total:$ ${calculoTot} </p>`
+}
+
+const agregadoraDeEventoDeBoton = () => {
+	const botones = document.querySelectorAll('.boton-agregar')
+	const arrayBoton = Array.from(botones)
+
+	arrayBoton.forEach((boton) => {
+		boton.addEventListener('click', (event) => {
+			let id = event.target.parentNode.id
+
+			let opcion = opciones.find((el) => el.id == id)
+
+            carrito.push({...opcion})
+
+            console.log(carrito)
+            agregadoraCarrito()
+		})
+	})
+}
+
+const renderizador = () => {
+    opciones.forEach((opcion) => {
+        container.innerHTML += `<section class="product-card" id=${opcion.id}>
+				<img
+					src=${opcion.imagen}
+					alt=${opcion.ingredientes}
+				/>
+				<h2>${opcion.nom}</h2>
+				<p>${opcion.ingredientes}</p>
+				<span class="price">$${opcion.precio}</span>
+                <br><br>
+				<button class='boton-agregar'>Agregar al pedido</button>
+			</section>`
+	})
+	agregadoraDeEventoDeBoton()
+}
+renderizador()
